083b07613d15c69542b889f599fa957b31a07cb4
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2015-02-07 07:23:34 +0900

    add more tests

--- fmart_test.go
+++ fmart_test.go
@@ -2,20 +2,13 @@ package fmart
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 	"time"
 )
-
-func TestIssueInvoiceParams(t *testing.T) {
-	x := 5
-	p := &IssueInvoiceParams{}
-	errs := p.Errors()
-	if y := len(errs); y != x {
-		t.Errorf("expected %d errors, got: %d", x, y)
-	}
-}
 
 func TestIssueInvoice(t *testing.T) {
 	var res string
@@ -57,4 +50,143 @@ func TestIssueInvoice(t *testing.T) {
 	if id != "" {
 		t.Errorf("expected empty id, got: %s", id)
 	}
+}
+
+func TestModifyInvoice(t *testing.T) {
+	var res string
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, res)
+	}))
+	defer ts.Close()
+
+	APIEndpoint = ts.URL
+
+	res = "not-reached"
+	p := &ModifyInvoiceParams{}
+	err := ModifyInvoice(p)
+	if err != ErrInvalidParams {
+		t.Errorf("expected ErrInvalidParams error, got: nil")
+	}
+
+	res = "invoice-1234"
+	p = &ModifyInvoiceParams{
+		ID:           res,
+		Name:         "松本行弘",
+		NameKatakana: "マツモトヒロユキ",
+		PhoneNumber:  "0120-444-444",
+		Amount:       100,
+		Expiry:       time.Now().AddDate(0, 0, 1),
+	}
+	err = ModifyInvoice(p)
+	if err != nil {
+		t.Errorf("expected nil error, got: %v", err)
+	}
+
+	res = "-1\nerror message"
+	err = ModifyInvoice(p)
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func TestCancelInvoice(t *testing.T) {
+	var res string
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, res)
+	}))
+	defer ts.Close()
+
+	APIEndpoint = ts.URL
+
+	res = "invoice-1234"
+	err := CancelInvoice(res)
+	if err != nil {
+		t.Errorf("expected nil error, got: %v", err)
+	}
+
+	res = "-1\nerror message"
+	err = CancelInvoice(res)
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func TestAckInvoiceStatuses(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		b, _ := ioutil.ReadAll(r.Body)
+
+		e := "1\r\n2\r\n3"
+		a := string(b)
+		if e != a {
+			t.Errorf("expected %v, got: %v", e, a)
+		}
+	}))
+	defer ts.Close()
+
+	APIEndpoint = ts.URL
+
+	err := AckInvoiceStatuses([]string{"1", "2", "3"})
+	if err != nil {
+		t.Errorf("expected nil error, got: %v", err)
+	}
+}
+
+func TestParseInvoiceStatuses(t *testing.T) {
+	var assertion func([]*InvoiceStatus, error)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assertion(ParseInvoiceStatuses(r))
+	}))
+	defer ts.Close()
+
+	assertion = func(statuses []*InvoiceStatus, err error) {
+		if err != ErrUnauthorizedRequest {
+			t.Errorf("expected ErrUnauthorizedRequest error, got nil")
+		}
+	}
+	http.PostForm(ts.URL, url.Values{
+		"login_user_id":  {"invalid_user_id"},
+		"login_password": {"invalid_password"},
+	})
+
+	assertion = func(statuses []*InvoiceStatus, err error) {
+		if err != ErrInvalidRequest {
+			t.Errorf("expected ErrInvalidRequest error, got nil")
+		}
+	}
+	http.PostForm(ts.URL, url.Values{
+		"login_user_id":     {""},
+		"login_password":    {""},
+		"number_of_notify":  {"1"},
+		"receipt_no_0000":   {"1"},
+		"status_0000":       {"4"},
+		"receipt_date_0000": {"201502082012"},
+		"payment_0000":      {"100"},
+	})
+
+	assertion = func(statuses []*InvoiceStatus, err error) {
+		if err != nil {
+			t.Errorf("expected nil error, got: %v", err)
+		}
+
+		if n := len(statuses); n != 3 {
+			t.Errorf("expected to get 3 statuses, got: %v", n)
+		}
+	}
+	http.PostForm(ts.URL, url.Values{
+		"login_user_id":     {""},
+		"login_password":    {""},
+		"number_of_notify":  {"3"},
+		"receipt_no_0000":   {"invlice-1"},
+		"status_0000":       {"1"},
+		"receipt_date_0000": {"201502082010"},
+		"payment_0000":      {"101"},
+		"receipt_no_0001":   {"invlice-2"},
+		"status_0001":       {"2"},
+		"receipt_date_0001": {"201502082010"},
+		"payment_0001":      {"102"},
+		"receipt_no_0002":   {"invlice-3"},
+		"status_0002":       {"3"},
+		"receipt_date_0002": {"201502082010"},
+		"payment_0002":      {"103"},
+	})
 }
